e993b9cb50bc8bf9e29676ea288eca75360b492c
Quentin <kwangz.bu@gmail.com>
2019-06-18 10:27:47 +0700

    Added first terrains

---
+++ AircraftRecognition/Assets/Scripts/PlanesSpawn.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanesSpawn : MonoBehaviour {
+
+    private Vector3 spawnPos;
+    private Quaternion spawnRot;
+    public Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        spawnPos = rb.position;
+        spawnRot = rb.rotation;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        StartCoroutine(ReplacePlane());
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    IEnumerator ReplacePlane()
+    {
+        while(true)
+        {
+            Debug.Log("ReplacePlane");
+
+            rb.transform.position = RandomPointOnCircleEdge(100);
+            Debug.Log("x" + rb.transform.position.x);
+            Debug.Log("y" + rb.transform.position.y);
+
+            yield return new WaitForSeconds(7);
+        }
+
+    }
+
+    private Vector3 RandomPointOnCircleEdge(float radius)
+    {
+        var vector2 = UnityEngine.Random.insideUnitCircle.normalized * radius;
+        return new Vector3(vector2.x + 350, 300, vector2.y + 300);
+    }
+}
---
+++ AircraftRecognition/Assets/Scripts/VelocityManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocityManager : MonoBehaviour {
+
+    public Vector3 move = new Vector3(0, 0, 200);
+    public Rigidbody rb;
+
+    // Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = move;
+    }
+
+    private void FixedUpdate()
+    {
+    }
+}
